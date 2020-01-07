7d8f798c39d6019b1356cf9846ad2e4b5448f462
user.name : "sacha DATA <user.name : sacha DATA>
2020-01-07 09:58:37 +0100

    mise en prod static
    suppression beta js

--- peapp/static/pinetest.js
+++
@@ -1,9 +0,0 @@
-var myQuest = new XMLHttpRequest()
-
-myQuest.open('GET','https://learnwebcode.github.io/json-example/animals-1.json')
-
-myQuest.onload = function(){
-  var data = JSON.parse(myQuest.responseText);
-  console.log(data[0])
-};  
-myQuest.send();
